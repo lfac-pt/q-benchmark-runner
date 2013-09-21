a71b2e28c7d2d3d068fa48767a113667906ec84f
Luis Cardoso <luis.cardoso@feedzai.com>
2013-09-21 23:06:05 +0100

    Implements a setup scripts that clones q into this folder. Adapts the rest of the runner.

--- run.sh
+++ run.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
+cd q
+
 #Prepare the necessary folders
 git checkout master
-(rm -rf benchmarkRunner/results)
-(mkdir benchmarkRunner/results)
+(rm -rf ../results)
+(mkdir ../results)
 (rm -rf benchmarkRunnerTemp)
 (mkdir benchmarkRunnerTemp)
 (cp -r benchmark/ benchmarkRunnerTemp)
 
 #Write the index.csv header
-echo "version" > benchmarkRunner/results/index.csv
+echo "version" > ../results/index.csv
 
 while read VERSION; do
-	echo $VERSION >> benchmarkRunner/results/index.csv
+	echo $VERSION >> ../results/index.csv
 
    	echo  "Benchmarking $VERSION"
 
 	#Run the benchmarks
 	git checkout $VERSION
-	(echo "ts,suite,test,ops" && ./node_modules/matcha/bin/matcha benchmarkRunnerTemp/compare-with-callbacks.js benchmarkRunnerTemp/scenarios.js -R csv) > benchmarkRunner/results/$VERSION.csv
+	(echo "ts,suite,test,ops" && ./node_modules/matcha/bin/matcha benchmarkRunnerTemp/*.js -R csv) > ../results/$VERSION.csv
 done
 
 #Go back to master and clean up benchmarkRunnerTemp
---
+++ setup.sh
@@ -0,0 +1,10 @@
+(rm -rf q)
+mkdir q
+git clone https://github.com/kriskowal/q.git
+cd q
+npm install
+
+#It would be nice for Q to upgrade to this version
+npm install matcha@0.3
+
+echo "Done!"
